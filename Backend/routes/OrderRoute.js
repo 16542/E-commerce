89aa3b07dc21687abfd606f3a320a91af4f60c5a
ahmed ben kahla <86633739+16542@users.noreply.github.com>
2022-08-09 12:40:33 +0100

    Update OrderRoute.js

--- Backend/routes/OrderRoute.js
+++ Backend/routes/OrderRoute.js
@@ -58,7 +58,7 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
 // //GET ALL
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
   try {
-    const orders = await Cart.find();
+    const orders = await Order.find();
     res.status(200).json(orders);
   } catch (err) {
     res.status(500).json(err);
